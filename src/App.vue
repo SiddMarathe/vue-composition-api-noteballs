06586e1878d093ba55dbea360f9294a4e2c746e2
Sid <siddharth@nukon.com>
2023-12-27 08:46:31 +0530

    init, router, home, stats

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="wrapper">
+    <RouterLink to="/">Notes</RouterLink>|
+    <RouterLink to="/stats">stats</RouterLink>
+    <RouterView />
+  </div>
+</template>
+<script setup></script>
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="notes-view">
+    <h1>Notes</h1>
+  </div>
+</template>
+
+<style scoped></style>
