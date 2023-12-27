927d0970d4fd72bb96c24f3e2b99778c464cc39f
Sid <siddharth@nukon.com>
2023-12-27 09:53:19 +0530

    vuetify added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <RouterLink to="/">Notes</RouterLink>|
-    <RouterLink to="/stats">stats</RouterLink>
+    <RouterLink class="v-btn" to="/">Notes</RouterLink>|
+    <RouterLink class="v-btn" to="/stats">stats</RouterLink>
     <RouterView />
   </div>
 </template>
