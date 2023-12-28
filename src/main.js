677e40ead316f128cea7130549827dd59334f3c3
Sid <siddharth@nukon.com>
2023-12-28 06:20:43 +0530

    fixing icons and styles in vue

--- src/main.js
+++ src/main.js
@@ -7,8 +7,20 @@ import router from '@/router/index.js'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { mdi } from 'vuetify/iconsets/mdi'
+import { aliases } from 'vuetify/iconsets/fa'
+import 'vuetify/styles' // Global styles
 
+const icons = {
+  defaultSet: 'mdi',
+  aliases,
+  sets: {
+    mdi
+  }
+}
 const vuetify = createVuetify({
+  icons,
   components,
   directives
 })
