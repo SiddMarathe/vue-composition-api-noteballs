8cd7288097b6ae5e788a3998eaf3c5d208ed730e
Sid <siddharth@nukon.com>
2024-01-07 18:01:02 +0530

    adding and removing event listeners to an element

--- src/components/notes/ModalDeleteNote.vue
+++ src/components/notes/ModalDeleteNote.vue
@@ -1,15 +1,15 @@
 <template>
   <v-card>
     <v-card-title>Delete Note?</v-card-title>
-    <v-card-text> Are you sure you wan to delete this note? </v-card-text>
+    <v-card-text> Are you sure you wan to delete this note?</v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn variant="flat" color="error" @click="handleDelete">Delete</v-btn>
+      <v-btn variant="flat" color="red lighten-1" @click="handleDelete">Delete</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { useNoteStore } from '@/store/notesStore.js'
 
 const dialog = ref(true)
@@ -27,6 +27,19 @@ const handleDelete = () => {
   notesStore.deleteNote(props.noteId)
   emits('close')
 }
+
+const keyboardControls = (e) => {
+  if (e.key === 'Enter') handleDelete()
+}
+
+onMounted(() => {
+  document.addEventListener('keyup', keyboardControls)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('keyup', keyboardControls)
+  //removing event listener is mandatory as it may persist even after the component is removed from the DOM
+})
 </script>
 
 <style scoped></style>
