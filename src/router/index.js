f1630c783f942abc14656bd1a5b22d1e00637ea8
Sid <siddharth@nukon.com>
2024-01-25 21:26:29 +0530

    adding auth guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import ViewNotes from '@/views/ViewNotes.vue'
 import ViewStats from '@/views/ViewStats.vue'
 import EditNote from '@/views/EditNote.vue'
 import viewAuth from '@/views/ViewAuth.vue'
+import { useAuthStore } from '@/store/authStore.js'
 
 export const routes = [
   {
@@ -31,4 +32,16 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach(async (to, from) => {
+  const authStore = useAuthStore()
+  if (!authStore.user.id) {
+    if (to.name !== 'auth') {
+      return { name: 'auth' }
+    }
+  } else {
+    if (to.name === 'auth') {
+      return false // Do not let the user navigate to another route
+    }
+  }
+})
 export default router
